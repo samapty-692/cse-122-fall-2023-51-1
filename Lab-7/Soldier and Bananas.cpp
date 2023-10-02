ce2c5d35d8e957add81c68650a537721f91b223e
Samapty Biswas <139231029+samapty-692@users.noreply.github.com>
2023-10-02 18:53:22 +0600

    Create Soldier and Bananas.cpp

---
+++ Lab-7/Soldier and Bananas.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int k,n,w;
+    cin>>k>>n>>w;
+    int cost=k*w*(w+1)/2;
+    if(n>=cost)
+        cout<<"0"<<endl;
+    else
+        cout<<cost-n<<endl;
+    return 0;
+}
+
